3b3d2d546247b926460cd752358ffd507cc13996
aleski.menard <aleski.menard@gmail.com>
2022-11-26 18:05:21 +0100

    fix smells

--- src/main/java/com/ipi/jva350/model/SalarieAideADomicile.java
+++ src/main/java/com/ipi/jva350/model/SalarieAideADomicile.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 @Entity
 public class SalarieAideADomicile {
 
-    public static float congesPayesAcquisParMois = 2.5f;
+    public static final float congesPayesAcquisParMois = 2.5f;
 
 
     @Id
@@ -20,7 +20,7 @@ public class SalarieAideADomicile {
 
     private String nom;
 
-    public static List<DayOfWeek> joursHabituellementTravailles = new ArrayList<>();
+    protected public static final List<DayOfWeek> joursHabituellementTravailles = new ArrayList<>();
 
     static {
         joursHabituellementTravailles.add(DayOfWeek.MONDAY);
